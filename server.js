d8d5ceb29a2ad20692b4cb0b99a3f2c9cb7bd00c
nozky <norvillie.villaruel@edu.sait.ca>
2021-04-14 17:32:14 -0600

    add gpio

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const app = express();
 app.use(cors())
 app.use(express.static(path.join(__dirname, '/public')))
 
+app.get('/on',(req,res)=>{
+  console.log('Turning on')
+  res.send('Turning on...')
+})
+
+app.get('/off',(req,res)=>{
+  console.log('Turning off')
+  res.send('Turning off...')
+})
+
+app.get('*',(req, res)=>{
+  res.send(`<h1>No page here. Everyone's laughing at you. :) (404)</h1>`)
+})
+
 app.listen(3000, ()=> {
   console.log('Listening on port 3000 on raspberry pi')
 })
