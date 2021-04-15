131a5ce6a2f39fe17b3c04a769af99fbfeef1a2a
nozky <norvillie.villaruel@edu.sait.ca>
2021-04-14 22:41:37 -0600

    added end point

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const btnOff = document.querySelector('.btn-off');
+const btnOn = document.querySelector('.btn-on');
+
+btnOff.addEventListener('click',()=>{
+  fetch('/on')
+    .then(res => res.text())
+    .then( data => console.log(data))
+    .catch( err => console.log( err))
+})
+
+
+btnOn.addEventListener('click',()=>{
+  fetch('/off')
+  .then(res => res.text())
+  .then( data => console.log(data))
+  .catch( err => console.log( err))
+})
